b4bc82ed553b8b85ac1de38333ff41aebf88e728
Sergio Leal <sergio@arima.eu>
2015-07-31 12:47:01 +0200

    Change to LocationJpaRepository

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -1,9 +1,12 @@
 package com.guitar.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.guitar.db.model.Location;
 
 public interface LocationJpaRepository extends JpaRepository<Location, Long>{
 
+	List<Location> findByStateLike(String stateName);
 }
